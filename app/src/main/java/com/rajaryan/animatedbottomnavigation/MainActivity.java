344f8e699af4351eb2b52418a1204deedb8afac4
Yash <yashrajmehta3027@gmail.com>
2020-09-18 23:20:45 +0530

    Initial commit

---
+++ app/src/main/java/com/rajaryan/animatedbottomnavigation/MainActivity.java
@@ -0,0 +1,46 @@
+package com.rajaryan.animatedbottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+public class MainActivity extends AppCompatActivity {
+    private ChipNavigationBar chipNavigationBar;
+    private Fragment fragment = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chipNavigationBar = findViewById(R.id.chipNavigation);
+
+        chipNavigationBar.setItemSelected(R.id.home, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new Home()).commit();
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch (i) {
+                    case R.id.home:
+                        fragment = new Home();
+                        break;
+                    case R.id.cart:
+                        fragment = new Cart();
+                        break;
+                    case R.id.profile:
+                        fragment = new Profile();
+                        break;
+
+                }
+
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                }
+            }
+        });
+    }
+}
